cc6b12817715f0616c0733c2e4c02310a579d6c8
Colten Appleby <colten96@gmail.com>
2021-02-14 21:44:16 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,9 @@
 def mario(phrase = "It's-a me, Mario!")
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts phrase
+  return phrase
 end
 
-def toadstool
+def toadstool(mario)
   puts status
 end
