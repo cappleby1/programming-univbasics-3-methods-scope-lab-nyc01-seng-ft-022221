70dbb90319d59ea50fec81dc3d465885fb477a57
Colten Appleby <colten96@gmail.com>
2021-02-14 21:52:08 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -4,8 +4,13 @@ def mario(phrase = "It's-a me, Mario!")
   return phrase
 end
 
-$status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+#$status = 'Thank You Mario! But Our Princess Is In Another Castle!'
 
-def toadstool()#status = 'Thank You Mario! But Our Princess Is In Another Castle!')
+def toadstool(status = 'Thank You Mario! But Our Princess Is In Another Castle!')
   puts status
+  return status
 end
+
+def link(mario1 = mario, toadstool1 = toadstool)
+  puts mario1
+  puts toadstool1
